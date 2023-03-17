b5342c5b48a5a8ed6f18569ed4b2411124f57a4f
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-17 18:48:54 -0500

    .

--- JavaNetBeans/src/main/java/com/mycompany/interfaz/CalculoAreas2.java
+++ JavaNetBeans/src/main/java/com/mycompany/interfaz/CalculoAreas2.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author castr
  */
-public class CalculoAreas extends javax.swing.JFrame {
+public class CalculoAreas2 extends javax.swing.JFrame {
     
     /**
      * Creates new form CalculoAreas
      */
-    public CalculoAreas() {
+    public CalculoAreas2() {
         initComponents();
     }
     
@@ -100,7 +100,6 @@ public class CalculoAreas extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
         jLabel3.setText("Configuraciones");
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         jLabel4.setText("Forma");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -281,20 +280,23 @@ public class CalculoAreas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CalculoAreas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CalculoAreas2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CalculoAreas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CalculoAreas2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CalculoAreas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CalculoAreas2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CalculoAreas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CalculoAreas2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CalculoAreas().setVisible(true);
+                new CalculoAreas2().setVisible(true);
             }
         });
     }
--- JavaNetBeans/src/main/java/com/mycompany/interfaz/SueldoProfesor.java
+++ JavaNetBeans/src/main/java/com/mycompany/interfaz/SueldoProfesor.java
@@ -12,14 +12,13 @@ import javax.swing.JOptionPane;
  * @author castr
  */
 public class SueldoProfesor extends javax.swing.JFrame {
-    
-    static int vhProfesional = 50000;
 
     /**
      * Creates new form SueldoProfesor
      */
     public SueldoProfesor() {
         initComponents();
+        this.setLocationRelativeTo(this);
     }
 
     /**
@@ -47,6 +46,11 @@ public class SueldoProfesor extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         txtHorasTrabajadas = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        txtTotalDescuentos = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        txtSueldoBruto = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -107,6 +111,21 @@ public class SueldoProfesor extends javax.swing.JFrame {
 
         jLabel6.setText("Horas Trabajadas");
 
+        jButton2.setText("Cerrar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        txtTotalDescuentos.setEditable(false);
+
+        jLabel7.setText("Total Descuentos");
+
+        txtSueldoBruto.setEditable(false);
+
+        jLabel8.setText("Sueldo Bruto");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -114,38 +133,43 @@ public class SueldoProfesor extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(32, 32, 32)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jLabel3)
-                                    .addComponent(jLabel4))
-                                .addGap(24, 24, 24)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtDescuentoSalud, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(txtSueldoNeto, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel1)
-                                    .addComponent(jLabel6))
-                                .addGap(24, 24, 24)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtValorHora, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
-                                    .addComponent(txtDescuentoPension)
-                                    .addComponent(txtHorasTrabajadas))))
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel5)
-                        .addGap(18, 18, 18)
-                        .addComponent(cbEscolaridad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(144, 144, 144)
+                        .addComponent(jButton1)
+                        .addGap(51, 51, 51)
+                        .addComponent(jButton2))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(120, 120, 120)
-                        .addComponent(lbTitulo)))
-                .addContainerGap(30, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(202, 202, 202))
+                        .addGap(39, 39, 39)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lbTitulo)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(jLabel3)
+                                            .addComponent(jLabel2)
+                                            .addComponent(jLabel1)
+                                            .addComponent(jLabel6))
+                                        .addGap(24, 24, 24))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(jLabel7)
+                                            .addComponent(jLabel4)
+                                            .addComponent(jLabel8))
+                                        .addGap(26, 26, 26)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtSueldoNeto)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(txtValorHora, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+                                        .addComponent(txtDescuentoPension)
+                                        .addComponent(txtHorasTrabajadas)
+                                        .addComponent(txtDescuentoSalud, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+                                        .addComponent(txtTotalDescuentos))
+                                    .addComponent(txtSueldoBruto, javax.swing.GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel5)))
+                        .addGap(18, 18, 18)
+                        .addComponent(cbEscolaridad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(32, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -172,11 +196,21 @@ public class SueldoProfesor extends javax.swing.JFrame {
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtTotalDescuentos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel7))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtSueldoBruto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel8))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtSueldoNeto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
-                .addGap(28, 28, 28)
-                .addComponent(jButton1)
-                .addContainerGap(27, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addGap(28, 28, 28))
         );
 
         pack();
@@ -208,7 +242,7 @@ public class SueldoProfesor extends javax.swing.JFrame {
                     txtValorHora.setText(nf.format(vEscolaridad));
                 }
             case "Doctor" ->                 {
-                    vEscolaridad  = 70000;
+                    vEscolaridad  = 90000;
                     txtValorHora.setText(nf.format(vEscolaridad));
                 }
             default -> {
@@ -218,13 +252,64 @@ public class SueldoProfesor extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        
+        String escolaridad = (String) cbEscolaridad.getSelectedItem();
+        
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+        
+        int vEscolaridad = 0;
+        
+        switch (escolaridad) {
+            case "Profesional" ->                 {
+                    vEscolaridad  = 50000;
+                }
+            case "Especialista" ->                 {
+                    vEscolaridad  = 55000;
+                }
+            case "Magister" ->                 {
+                    vEscolaridad  = 60000;
+                }
+            case "Doctor" ->                 {
+                    vEscolaridad  = 90000;
+                }
+            default -> {
+            }
+        }
+        
+        double nHoras = Integer.parseInt(txtHorasTrabajadas.getText());
+        double desPension = 0.05;
+        double desSalud = 0.05;
+        
         try {
-            int hTrabajadas = Integer.parseInt(txtHorasTrabajadas.getText());
-            JOptionPane.showMessageDialog(rootPane,"");
+            if ((nHoras*vEscolaridad) > 1_300_000) {
+                
+                String descuentos = nf.format((nHoras*vEscolaridad)*desPension);
+                txtDescuentoPension.setText(descuentos);
+                txtDescuentoSalud.setText(descuentos);
+                String sueldo = nf.format((nHoras*vEscolaridad) - (nHoras*vEscolaridad)*0.10);
+                String tDescuentos = nf.format((nHoras*vEscolaridad)*0.10);
+                txtTotalDescuentos.setText(tDescuentos);
+                txtSueldoBruto.setText(nf.format(nHoras*vEscolaridad));
+                txtSueldoNeto.setText(sueldo);
+                
+            } else {
+                txtDescuentoPension.setText(nf.format(0));
+                txtDescuentoSalud.setText(nf.format(0));
+                String sueldo = (nf.format(nHoras*vEscolaridad));
+                String tDescuentos = nf.format(0);
+                txtTotalDescuentos.setText(tDescuentos);
+                txtSueldoBruto.setText(sueldo);
+                txtSueldoNeto.setText(sueldo);
+            }
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(rootPane, "Ha ocurrido un error : " + e);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -264,19 +349,24 @@ public class SueldoProfesor extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cbEscolaridad;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JLabel lbTitulo;
     private javax.swing.JTextField txtDescuentoPension;
     private javax.swing.JTextField txtDescuentoSalud;
     private javax.swing.JTextField txtHorasTrabajadas;
+    private javax.swing.JTextField txtSueldoBruto;
     private javax.swing.JTextField txtSueldoNeto;
+    private javax.swing.JTextField txtTotalDescuentos;
     private javax.swing.JTextField txtValorHora;
     // End of variables declaration//GEN-END:variables
 }
